6ed2713c0c3eb97a862b5d27e9216baa2cc37175
RoryEiffe <roryeiffe8@gmail.com>
2023-12-06 17:17:12 -0500

    Added enzyme tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,66 +1,33 @@
-import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import Counter from '../components/Counter';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-// Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
-// unmount and cleanup DOM after the test is finished.
-afterEach(cleanup);
+Enzyme.configure({ adapter: new Adapter() });
 
-it('Should render', () => {
-    const element = render(
-        <Counter />,
-    );
-});
-
-it('Should initially display 0', () => {
-    const element = render(
-        <Counter />,
-    );
-
-    let text = element.baseElement.querySelector('p').textContent;
-
-    expect(text).toBe('You clicked 0 times');
-});
-
-it('should increment the count', () => {
-    const element = render(
-        <Counter />,
-    );
-
-    let button = element.baseElement.querySelector('button#increment');
-
-    fireEvent.click(button);
-
-    let text = element.baseElement.querySelector('p').textContent;
-
-    expect(text).toBe('You clicked 1 times');
-
+// Test that component is rendered:
+it('Counter component is rendered', () => {
+    const wrapper = mount(<Counter />);
+    expect(wrapper.exists()).toBe(true);
 })
 
-it('should decrement the count', () => {
-    const element = render(
-        <Counter />,
-    );
-
-    let button = element.baseElement.querySelector('button#decrement');
-
-    fireEvent.click(button);
-
-    let text = element.baseElement.querySelector('p').textContent;
-
-    expect(text).toBe('You clicked -1 times');
+it('should start at 0:', () => {
+    const wrapper = mount(<Counter />);
+    const element = wrapper.find('p');
+    expect(element.text()).toBe('You clicked 0 times');
 })
 
-it('should update the title', () => {
-    const element = render(
-        <Counter />,
-    );
+// Test that increment button works:
+it('should increment by 1:', () => {
+    const wrapper = mount(<Counter />);
+    wrapper.find('button').at(0).simulate('click');
+    const element = wrapper.find('p');
+    expect(element.text()).toBe('You clicked 1 times');
+})
 
-    let button = element.baseElement.querySelector('button#increment');
-
-    fireEvent.click(button);
-    fireEvent.click(button);
-    fireEvent.click(button);
-    fireEvent.click(button);
-
-    expect(document.title).toBe('You clicked 4 times');
+// Test that decrement button works:
+it('should decrement by 1:', () => {
+    const wrapper = mount(<Counter />);
+    wrapper.find('button').at(1).simulate('click');
+    const element = wrapper.find('p');
+    expect(element.text()).toBe('You clicked -1 times');
 })
